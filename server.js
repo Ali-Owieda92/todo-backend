36e66edbfa254d3c80101c17ac396e41a469242b
Ali Owieda <owiedaali92@gmail.com>
2025-12-29 17:04:16 +0200

    Just add cors to make client can access on the endpoints

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
-import connectDB from "./config/db.js";
+import connectDB from "./config/db.js"
 import task from "./routes/task.js"
+import cors from "cors"
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,15 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req,res) => {
     return res.status(200).json({msg: "Your APIs run well"});
